080b5384d3953599043f5d749f7a77e9b2010490
kekw99 <utsahakarki3@gmail.com>
2025-06-16 21:27:41 +0545

    added comments to login.h

--- login.h
+++ login.h
@@ -3,29 +3,49 @@
 
 #include <QDialog>
 
+// Forward declaration of the UI class generated from the .ui file by Qt's uic
 namespace Ui {
 class login;
 }
 
+// login class inherits from QDialog to provide a login dialog window
 class login : public QDialog
 {
     Q_OBJECT
 
 public:
+    // Constructor: initializes the login dialog, optionally with a parent widget
     explicit login(QWidget *parent = nullptr);
+
+    // Destructor: cleans up resources used by the dialog
     ~login();
 
 private slots:
+    // Slot called when the "Login" button is clicked
     void on_buttonLogin_clicked();
+
+    // Slot called when the "Signup" button is clicked
     void on_buttonSignup_clicked();
+
+    // Slot called when the "Forgot Password" button is clicked
     void on_buttonForgotPassword_clicked();
+
+    // Slot called when the "Toggle Password" (show/hide) button is clicked
     void on_buttonTogglePassword_clicked();
 
 private:
+    // Pointer to the UI elements defined in the .ui file
     Ui::login *ui;
+
+    // Utility function to show a message (e.g., error or info) to the user
     void showMessage(const QString &message);
+
+    // Boolean flag to track whether the password field is currently visible or hidden
     bool passwordVisible;
 
+    // Paths to the icons for the password visibility toggle button:
+    // iconClosedPath is the icon when password is hidden
+    // iconOpenPath is the icon when password is visible
     QString iconClosedPath;
     QString iconOpenPath;
 };
