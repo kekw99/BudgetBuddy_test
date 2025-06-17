579f0eb2719691c0287d72a0641df742bc5632d8
kekw99 <utsahakarki3@gmail.com>
2025-06-17 18:08:53 +0545

    added comments to mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,23 +1,31 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+// Include the base class for main application windows in Qt
 #include <QMainWindow>
 
+// Begin and end namespace macros to avoid naming conflicts
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
 
+// MainWindow class declaration, derived from QMainWindow
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+    Q_OBJECT  // Enables Qt's meta-object features like signals and slots
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    // Constructor - creates an instance of MainWindow
+    explicit MainWindow(QWidget *parent = nullptr);
+
+    // Destructor - cleans up any resources used by MainWindow
     ~MainWindow();
 
 private:
+    // Pointer to the UI class generated from mainwindow.ui by Qt Designer
     Ui::MainWindow *ui;
 };
+
 #endif // MAINWINDOW_H
