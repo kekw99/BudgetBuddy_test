ffe853a588c457f385fca94f3cc29413f046ad8e
kekw99 <utsahakarki3@gmail.com>
2025-06-17 21:33:01 +0545

    added comments to homepage.cpp

--- homepage.cpp
+++ homepage.cpp
@@ -1,13 +1,12 @@
 #include "homepage.h"
 #include "ui_homepage.h"
 #include "transactions.h"
-#include "monthlygoals.h"  // ✅ Include this!
+#include "monthlygoals.h"  // ✅ Include this header for monthly goals dialog
 #include <QMessageBox>
 #include "viewtransactions.h"
 #include "analyzereport.h"
 
-
-
+// Constructor: sets up the UI for the homepage dialog
 homepage::homepage(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::homepage)
@@ -15,34 +14,40 @@ homepage::homepage(QWidget *parent) :
     ui->setupUi(this);
 }
 
+// Destructor: cleans up the UI object
 homepage::~homepage()
 {
     delete ui;
 }
 
+// Slot called when the "Add Transaction" button is clicked
 void homepage::on_buttonAddTransaction_clicked()
 {
+    // Create and show the transactions dialog as modal
     transactions *txnWindow = new transactions(this);
     txnWindow->exec();
 }
 
+// Slot called when the "View Transaction" button is clicked
 void homepage::on_buttonViewTransaction_clicked()
 {
+    // Create and show the viewtransactions dialog as modal
     viewtransactions *vt = new viewtransactions(this);
     vt->exec();
 }
 
+// Slot called when the "Set Goal" button is clicked
 void homepage::on_buttonSetGoal_clicked()
 {
-    // ✅ Replaced message box with actual window
+    // Create and show the monthlygoals dialog as modal instead of a message box
     monthlygoals *goalWindow = new monthlygoals(this);
     goalWindow->exec();
 }
 
-
+// Slot called when the "Analyze Report" button is clicked
 void homepage::on_buttonAnalyzeReport_clicked()
 {
+    // Create and show the analyzereport dialog as modal
     analyzereport *report = new analyzereport(this);
     report->exec();
 }
-
